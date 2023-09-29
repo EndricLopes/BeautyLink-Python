f5ab038102f4fb89316b5b216cc4a66092d08ffd
Endric Lopes <endricayl@gmail.com>
2023-09-29 09:29:24 -0300

    invertendo cors

--- api/server-flask/server.py
+++ api/server-flask/server.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import os
 
 app = Flask(__name__)
-CORS(app, origins=['https://batida-de-ponto-api-flask.vercel.app', 'https://pagina-batida-ponto.vercel.app/'], supports_credentials=True)
+CORS(app, origins=['https://pagina-batida-ponto.vercel.app/','https://batida-de-ponto-api-flask.vercel.app' ], supports_credentials=True)
 
 # Configuração do banco de dados
 # Configuração do banco de dados
