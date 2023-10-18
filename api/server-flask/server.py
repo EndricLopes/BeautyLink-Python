7d74c112aa8fd8a67427cf63337783783442f482
Endric Lopes <endricayl@gmail.com>
2023-10-18 12:09:17 -0300

    api espelho

--- api/server-flask/server.py
+++ api/server-flask/server.py
@@ -214,8 +214,15 @@ def get_hora():
 def get_horas_trabalhadas():
     cursor = conexao.cursor(dictionary=True)
     cursor.execute("SELECT SEC_TO_TIME(SUM(IF(hora_saida1 < hora_entrada1, TIME_TO_SEC(TIMEDIFF(hora_saida1 + INTERVAL 24 HOUR, hora_entrada1)), TIME_TO_SEC(TIMEDIFF(hora_saida1, hora_entrada1))) +IF(hora_saida2 < hora_entrada2, TIME_TO_SEC(TIMEDIFF(hora_saida2 + INTERVAL 24 HOUR, hora_entrada2)), TIME_TO_SEC(TIMEDIFF(hora_saida2, hora_entrada2)))) - 120*60*60) AS 'Saldo Mensal'FROM  controle_pontoWHERE     fk_id_login_ponto = 26 AND    dia BETWEEN '2023-08-10' AND '2023-09-10';")    
-    saldo = cursor.fetchone()    
-    return jsonify({'Saldo': saldo})
+    saldo = cursor.fetchone()
+    cursor.execute("SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(hora_saida3, hora_entrada3)))) AS 'Horas Extras no Mês' FROM controle_ponto WHERE fk_id_login_ponto = 27 AND dia BETWEEN '2023-09-01' AND '2023-10-07';")
+    Horas_extra = cursor.fetchone()
+    cursor.execute("SELECT hora_entrada1, hora_saida2, dia FROM controle_ponto WHERE fk_id_login_ponto = 26 AND dia BETWEEN '2023-09-10' AND '2023-10-10';")    
+    result = cursor.fetchone()
+    Hora_entrada = result['Hora_entrada1']
+    Hora_saida = result['Hora_saida2']
+    Dia = result['dia']
+    return jsonify({'Saldo Mensal': saldo}, {'Horario entrada': Hora_entrada},{'Horario saida': Hora_saida}, {'Data': Dia}, {'Horas extra': Horas_extra})
 
 
 @app.route('/sua-rota')
